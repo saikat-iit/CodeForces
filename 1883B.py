54c381ce5a7b38a78e5f0651c3acc7620918a316
Saikat Das <sd.saikat369@gmail.com>
2024-01-08 17:11:34 +0530

    Added comments

--- 1883B.py
+++ 1883B.py
@@ -1,16 +1,19 @@
 from collections import Counter
 
+#To check odd variable
 def odd(i:int):
     if i%2 == 0: return False
     else: return True
 
+#To count No. of odd numbers in a list
 def cnt_odd(lst:list):
     cnt = 0
     for i in lst:
         if odd(i): cnt += 1
     return cnt
 
-def palindrome(lst:str):
+#To check Palindrome
+def palindrome(lst):
     cnt = cnt_odd(lst)
         
     if cnt > 1: return False
@@ -18,16 +21,18 @@ def palindrome(lst:str):
 
 for _ in range(int(input())):
     t = list(map(int, input().split()))
-    n, k = t[0], t[1]
-    s = input()
-    dict_s = dict(Counter(s))
+    n, k = t[0], t[1] #where n = length of character and k is no. of character to remove
+    s = input() 
+    dict_s = dict(Counter(s)) #This gives dictionary, with frequency of each letter as values 
     lst_value = list(dict_s.values())
     check = True
 
+    #Palindrome is possible only if n-k = odd then 1 odd and rest even frequency, if n-k is even then all even frequency
     while(check):
         if k == 0 or cnt_odd(lst_value) == 0:
             check = False
         
+        #removing any odd presence, using the k value
         for i in range(len(lst_value)):
             if odd(lst_value[i]) and k > 0:
                 lst_value[i] -= 1
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,4 @@
-import math
-for _ in range(int(input())):
-    n = list(map(int, input().split()))
-    a, b = n[0], n[1]
-    num = 0
-    if b%a == 0:
-        print(int(b**2/a))
-    else:
-        print(int(math.lcm(a,b)))
+from collections import Counter
+s = "saikat"
+d = dict(Counter(s))
+print(d)
